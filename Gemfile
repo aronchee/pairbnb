d7a03fb136582ccdd6ff55fb093db4c22043a89c
nextacademy <cheekokfoong@gmail.com>
2016-10-24 18:03:44 +0800

    done with user interface

--- Gemfile
+++ Gemfile
@@ -45,3 +45,7 @@ group :development do
   gem 'spring'
 end
 
+gem 'omniauth'
+gem 'omniauth-facebook'
+gem 'clearance'
+gem 'figaro'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+  resources :users, controller: "users", only: :show
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
   get 'welcome_pages/home'
-
   get 'welcome_pages/help'
-
   root 'welcome_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
