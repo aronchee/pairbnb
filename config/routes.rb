dc6d25149cc24bc9e2f79fa447ac3fabad18c334
nextacademy <cheekokfoong@gmail.com>
2016-10-24 14:38:04 +0800

    Created homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome_pages/help'
 
+  root 'welcome_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
